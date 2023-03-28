c178c843e4716e9220bd8964ec1b083eda3421b0
adedapoayo <adefowojuadedapo@gmail.com>
2023-03-28 15:49:31 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Return;: the line converted from the string
+ */
+int _atoi(char *s)
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] !- '\0')
+	len++;
+	while (i < len && f -- o)
+
+	if (s[i] -- '.')
+	++d;
+
+	if 
