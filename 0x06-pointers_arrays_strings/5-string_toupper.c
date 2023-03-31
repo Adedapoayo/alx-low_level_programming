7de5f20e86e600a6e82fafa60643c4b1de7e0d8f
adedapoayo <adefowojuadedapo@gmail.com>
2023-03-31 03:21:05 -0700

    5-string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * string_toupper - change all lower case to uppercase
- * @n: strings to be converted
+ * string_toupper - change lower case to upper case
+ * @str: string to be converted
  * Return: strings
  */
-char *string_toupper(char *)
+
+char *string_toupper(char *str)
 {
 	int i;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] - 32;
+	{ if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] = str[i] - 32;
 	}
 	return (str);
 }
