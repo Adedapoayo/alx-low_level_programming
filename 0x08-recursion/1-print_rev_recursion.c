2e02fbe96fd0dd7beaadf34bf33775a0047b11ea
adedapoayo <adefowojuadedapo@gmail.com>
2023-04-05 07:45:47 -0700

    3-factorial

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,13 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts ();
- * @s: input
- * Return: Always 0 (success)
+ * _ptint_rev_recursion - prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
+		_print_rev_recursion(s +1);
 		_putchar(*s);
-		_puts_recursion(s +1);
 	}
-
-	else
-		_putchar('\n');
 }
