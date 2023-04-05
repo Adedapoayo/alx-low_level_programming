5a899aadd60cea18b6ac3f93edbff8042ff9e69c
adedapoayo <adefowojuadedapo@gmail.com>
2023-04-05 14:40:41 -0700

    101-wildcmp.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,24 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n int i);
+int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * return: the resulting square root
+ * @n: number to calculate the square root of
+ * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return ( -1);
+		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
 /**
+ * actual_sqrt_recursion - recures to find the natural
  * square root of a number
+ * @n: number to calculate the square root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
  */
 int actual_sqrt_recursion(int n, int i)
 {
