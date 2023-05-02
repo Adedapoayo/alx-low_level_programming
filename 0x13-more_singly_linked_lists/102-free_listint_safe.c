06c2b21ec2ce5a384cea8374e31e3a690bd655bc
adedapoayo <adefowojuadedapo@gmail.com>
2023-05-02 14:16:26 -0700

    project

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,10 +1,9 @@
 #include "lists.h"
-
 /**
- * free_listnt_safe - frees a linked list
- * @h: pointer to the first npde in the linked list
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
  *
- * Return: number of elements in the freed list
+ * Return: number of element in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
@@ -14,7 +13,6 @@ size_t free_listint_safe(listint_t **h)
 
 	if (!h || !*h)
 		return (0);
-
 	while (*h)
 	{
 		diff = *h - (*h)->next;
@@ -33,8 +31,6 @@ size_t free_listint_safe(listint_t **h)
 			break;
 		}
 	}
-
 	*h = NULL;
-
 	return (len);
 }
