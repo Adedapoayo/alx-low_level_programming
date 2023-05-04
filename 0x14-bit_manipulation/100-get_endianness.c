466a9115c6e00d1839e82d67577e727f057e87ec
adedapoayo <adefowojuadedapo@gmail.com>
2023-05-04 02:04:11 -0700

    project

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - checkes if a machine is little or big
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
